8f0603becfa2bd345ee23cc9a07dcd0352f0b3a1
ArnaudSushi <arnaud.vadez@gmail.com>
2021-12-23 13:36:01 +0100

    calc fini

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  firstNum: number = 0;
+  secondNum: number = 0;
+  operator: string = "";
+  result: number = 0;
+  showResult: boolean = false;
+  calculus: string = "";
+  previousCalculus: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calculate(): void {
+    if (this.operator) {
+      let time = new Date();
+      this.calculus = time.toLocaleString('fr-FR') + " : ";
+    }
+    this.showResult = true;
+    switch (this.operator) {
+      case "add":
+        this.result = this.firstNum + this.secondNum;
+        this.calculus += this.firstNum + " + " + this.secondNum + " = " + this.result;
+        this.previousCalculus.push(this.calculus);
+      break;
+      case "sub":
+        this.result = this.firstNum - this.secondNum;
+        this.calculus += this.firstNum + " - " + this.secondNum + " = " + this.result;
+        this.previousCalculus.push(this.calculus);
+      break; 
+      case "mul":
+        this.result = this.firstNum * this.secondNum;
+        this.calculus += this.firstNum + " * " + this.secondNum + " = " + this.result;
+        this.previousCalculus.push(this.calculus);
+      break;
+      case "div":
+        this.result = this.firstNum / this.secondNum;
+        this.calculus += this.firstNum + " / " + this.secondNum + " = " + this.result;
+        this.previousCalculus.push(this.calculus);
+      break;
+      default:
+        this.showResult = false;
+        break;
+    }
+  }
+
+  erasePrevCalcs(): void {
+    this.previousCalculus = [];
   }
 
 }
